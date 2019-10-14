1747f0f4735f49ba14684486afbe32fee7ddffc3
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-14 14:04:22 +0100

    :sparkles: Complete gate tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
+// Test away
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Controls />);
+});
+
+describe("Controls Component", () => {
+    it("Controls show", () => {})
+})
